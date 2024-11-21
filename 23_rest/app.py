8d0bb49c5441d2f1bfa9b4a5b5ccea6f38c03923
alee51 <alee51@stuy.edu>
2024-11-21 10:32:16 -0500

    updated

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    with open('key_nasa.txt', 'r') as f:
-        nasa_api_key = f.read().strip()   
+    with open('key_nasa.txt', 'r') as f:    # open key file
+        nasa_api_key = f.read().strip()     # read key file
     base_url = "https://api.nasa.gov/planetary/apod"
     params = urllib.parse.urlencode({'api_key': nasa_api_key})
     full_url = f"{base_url}?{params}"       # base url + api key
     # print(full_url)
     with urllib.request.urlopen(full_url) as text:
         # print(text.read())
-        apod_data = json.loads(text.read().decode('utf-8'))         # json object string -> python dict
+        apod_data = json.loads(text.read())         # json object string -> python dict
     # print(apod_data)
     # retrieving data from dictionary
     image_url = apod_data.get('url', '')
