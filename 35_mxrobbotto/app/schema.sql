16c41c3d92b69efb4cbfadec646a5c16a58cb5ed
alee51 <alee51@stuy.edu>
2025-03-13 11:45:49 -0400

    fixing mistakes

--- 35_mxrobbotto/app/schema.sql
+++ 35_mxrobbotto/app/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE stories (
+CREATE TABLE IF NOT EXISTS stories (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     creator_id INTEGER NOT NULL,
     FOREIGN KEY (creator_id) REFERENCES users (id)
 );
 
-CREATE TABLE contributions (
+CREATE TABLE IF NOT EXISTS contributions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     story_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
